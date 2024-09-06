db6a5eeca198d6dd0b4acdcfc7fc3f44597dd233
Yashasvi Chandra <146069599+Yasha5vi@users.noreply.github.com>
2024-09-06 09:28:47 +0530

    week4-2.c

---
+++ week4-2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
+int main(){
+    pid_t pid;
+    pid = fork();
+    if(pid == 0){
+        while(1);
+        sleep(50);
+    }else{
+        exit(0);
+    }
+    return 0;
+}
